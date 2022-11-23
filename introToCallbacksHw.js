1b51c9fe45707ce6d3d4873b878302f277eef49e
Thomas Conger <t.e.conger@gmail.com>
2022-11-23 14:44:26 -0800

    bound this to _tick when invoking in set interval and added cosmetic flourishes in printTime

--- introToCallbacksHw.js
+++ introToCallbacksHw.js
@@ -8,21 +8,49 @@
 
 class Clock{
   constructor(){
-    let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    let second = date.getSeconds();
+    this.date = new Date();
+    this.hour = this.date.getHours();
+    this.minute = this.date.getMinutes();
+    this.second = this.date.getSeconds();
     this.printTime();
-    setInterval(this._tick, 1000);
+    setInterval(this._tick.bind(this), 1000);
   }
   printTime(){
-    console.log(`${this.hour}:${this.minute}:${this.second}`);
+    let second = this.second
+    let minute = this.minute
+    let hour = this.hour
+
+    if (second < 10) {
+      second = `0${second}`
+    }
+
+    if (minute < 10) {
+      minute = `0${minute}`
+    }
+
+    if (hour < 10) {
+      hour = `0${hour}`
+    }
+
+    console.log(`${hour}:${minute}:${second}`);
   }
 
   _tick(){
     this.second++;
-    printTime.call()
+    if (this.second >= 60) {
+      this.second -= 60;
+      this.minute += 1;
+    }
+    if (this.minute >= 60) {
+      this.minute -= 60;
+      this.hour += 1;
+    }
+      if (this.hour >= 12) {
+      this.hour -= 12;
+    }
     this.printTime();
   }
 }
 let timer = new Clock();
+
+// what we learned: when you pass in a function as a callback, it loses context for this, which means it must be bound
